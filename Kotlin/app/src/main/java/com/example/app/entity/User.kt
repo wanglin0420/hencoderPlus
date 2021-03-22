b785a01b496b6db3a0824b8d087a33629be41c23
wangling <wl_0420@163.com>
2021-03-22 16:50:14 +0800

    1

---
+++ Kotlin/app/src/main/java/com/example/app/entity/User.kt
@@ -0,0 +1,8 @@
+package com.example.app.entity
+
+/**
+ *Author:wangling
+ *Email:wl_0420@163.com
+ *Date:3/22/21 4:17 PM
+ */
+data class User(val username: String, val password: String, val code: String)
